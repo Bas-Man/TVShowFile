a307bc542795ad4a112fe12d35c82567b973831f
Adam Spann <adam_spann@hotmail.com>
2018-11-27 00:20:05 +0900

    Moved test files to test directory

--- tvshowfile/patterns.py
+++ tvshowfile/patterns.py
@@ -1,21 +1,35 @@
 
-patterns =[
-    r"""
-	(?P<ShowNameA>.*)(?=(?:[. ](?:[S][0-9]{2})|[0-9][x])) #Show name
-	(?: # Get year if present
-	[ .]?(?<=[.( ](?P<Year>\d{4})[.) ])
-	| # Get Season
-	[. ](?:S(?P<ShowSeason>\d{1,2}))[E|X]
-	)
-	(?: # Get Episodes in case multi episode file
-	(?P<FirstEpisode>[0-9]{1,2})(?:[E])(?P<LastEpisode>[0-9]{1,2})
-	| # Get single Eposide if single episode
-	(?P<Episode>[0-9]{1,2})
-	)
-	[.] # Finished getting Seasons and Episodes
-	(?:.*) # Get everything else but not the file extension
-	[.]?(?<=(?P<FileExt>[a-z]{3}|[a-z]{2}[0-9]{1}) # Get the file extension
-	)
-	"""
- ]
+# coding=utf8
 
+regex_SXEX = r"""
+(?P<showname>.*)(?=(?:[. ](?:[S][0-9]{2})|[0-9][x])) #Show name
+(?: # Get year if present
+[ .]?(?<=[.( ](?P<showyear>\d{4})[.) ])
+| # Get Season
+[. ](?:S(?P<showseason>\d{1,2}))[E|X]
+)
+(?: # Get Episodes in case multi episode file
+(?P<firstepisode>[0-9]{1,2})(?:[E])(?P<lastepisode>[0-9]{1,2})
+| # Get single Eposide if single episode
+(?P<episode>[0-9]{1,2})
+)
+[.] # Finished getting Seasons and Episodes
+(?:.*) # Get everything else but not the file extension
+[.]?(?<=(?P<fileext>[a-z]{3}|[a-z]{2}[0-9]{1}) # Get the file extension
+)
+"""
+
+test_SXEX = ("v.2009.S01E13.the.title.avi\n"
+"Se7en.(1995).S01E01.blah.avi\n"
+"Arrow.S01E01.blah1.avi\n"
+"Castle.(2015).S01E01.avi\n"
+"Castle (2015) S01x03.avi\n"
+"Castle.2015.S01E10.avi\n"
+"Castle.S01E22.avi\n"
+"Castle.(2015).S02E11.avi\n"
+"Castle (2015) S02E12.avi\n"
+"S.W.A.T.S01E02.720p.avi\n"
+"S.W.A.T.S02E02E03.The.Dash.720p.avi\n"
+"S.W.A.T.2018.S02E04.The.Dash.avi\n"
+"S.W.A.T.(2018).S02E01.Title.720p.avi\n"
+"Doctor Who (2005).S01E01.1080p.mp3")
