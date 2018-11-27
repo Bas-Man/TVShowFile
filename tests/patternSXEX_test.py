1ff1a3849e0f922e5e642727d991e85c3e594286
Adam Spann <adam_spann@hotmail.com>
2018-11-27 23:55:48 +0900

    Added tests for hasYear and isMultiEpisode functions

--- tests/patternSXEX_test.py
+++ tests/patternSXEX_test.py
@@ -14,11 +14,25 @@ class TVShowFileParserTests(unittest.TestCase):
         self.assertEqual(self.filename.showName,"test")
         self.assertEqual(self.filename.season, "01")
         self.assertEqual(self.filename.episode,"01")
-        self.assertIsNone(self.filename.seasonEpisode)
+        self.assertEqual(self.filename.seasonEpisode, "S01E01")
         self.assertEqual(self.filename.fileExt,"avi")
-        
+        self.assertIsNone(self.filename.firstEpisode)
+        self.assertIsNone(self.filename.lastEpisode)
+        self.assertIsNone(self.filename.year)
+        print("\nName: " + self.filename.getShowName())
+        print("Season: " + self.filename.getSeason())
+        print("Episode: " + self.filename.getEpisode())
+        print("SeasonEpisode: " + self.filename.getSeasonEpisode())
+        print("Ext: " + self.filename.getFileExt())
+
     def testgetShowData(self):
         self.assertTrue(self.filename.getShowData())
+
+    def testhasYear(self):
+        self.assertFalse(self.filename.year, "Year is False as filename does not contain a Year")
+
+    def testisMultiEpisode(self):
+        self.assertFalse(self.filename.multiEpisode)
     
 if __name__ == '__main__':
     unittest.main()
