c9a90c2590ee2d5a0f912f0255f78ff0a7f164d5
Adam Spann <adam_spann@hotmail.com>
2018-11-27 23:56:26 +0900

    This file handles testing for S00EXXEXX with YEAR filenames

---
+++ tests/patternSXEXEX_Year_test.py
@@ -0,0 +1,41 @@
+import unittest
+from tvshowfile import TVShowFile
+
+
+class TVShowFileParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = TVShowFile.TVShowFileParser("test.2018.S01E01E02.avi")
+
+    def tearDown(self):
+        self.filename = None
+
+    def testObjValuesSet(self):
+        self.assertEqual(self.filename.showName,"test.2018")
+        self.assertEqual(self.filename.season, "01")
+        self.assertIsNone(self.filename.episode)
+        self.assertEqual(self.filename.firstEpisode,"01")
+        self.assertEqual(self.filename.lastEpisode, "02")
+        self.assertEqual(self.filename.seasonEpisode, "S01E01E02")
+        self.assertEqual(self.filename.fileExt,"avi")
+        self.assertEqual(self.filename.year, "2018")
+        
+        print("\nName: " + self.filename.getShowName())
+        print("Season: " + self.filename.getSeason())
+        print("First Episode: " + self.filename.getFirstEpisode())
+        print("Last Episode: " + self.filename.getLastEpisode())
+        print("SeasonEpisode: " + self.filename.getSeasonEpisode())
+        print("Ext: " + self.filename.getFileExt())
+        print("Year: " + self.filename.getYear())
+
+    def testgetShowData(self):
+        self.assertTrue(self.filename.getShowData())
+
+    def testhasYear(self):
+        self.assertTrue(self.filename.hasYear())
+
+    def testisMultiEpisode(self):
+        self.assertTrue(self.filename.isMultiEpisode())
+    
+if __name__ == '__main__':
+    unittest.main()
