5dedd4dd94b29779e52b2111eed7b43b3013301d
Adam Spann <adam_spann@hotmail.com>
2018-11-29 22:57:04 +0900

    Created sample1.py to show possible use of module.

--- sample/sample1.py
+++ sample/sample1.py
@@ -3,11 +3,36 @@
 import sys
 sys.path.insert(0, "..")
 
-import tvshowfile
+import tvshowfile.tvshowfile
 
-obj = tvshowfile.TVShowFile.TVShowFileParser("test.avi")
+fail = tvshowfile.tvshowfile.TVShowFileParser("test.avi")
+succ = tvshowfile.tvshowfile.TVShowFileParser("test.S01E01.avi")
+succ2 = tvshowfile.tvshowfile.TVShowFileParser("test.S01E01.1080p.avi")
 
-if obj is False:
-    print("nothing here\n")
-else:
-    print(obj.filename + "\n")
+# Example 1 testing that file was able to be parsed or not
+if not fail.wasParsed():
+    print("Unable to parse file: " + fail.getFilename())
+
+# Example 1 Successful parsing of file
+if succ.wasParsed:
+    print("Able to parse file: " + succ.getFilename())
+    print("File: " + succ.getFilename())
+    print("Name: " + succ.getShowName())
+    print("Season: " + succ.getSeason())
+    print("Episode: " + succ.getEpisode())
+    print("SeasonEpisode: " + succ.getSeasonEpisode())
+    print("Quality: " + succ.getQuality())
+    print("Ext: " + succ.getFileExt())
+
+print("\n\n")
+
+# Example 2 Successful parsing of file
+if succ.wasParsed:
+    print("Able to parse file: " + succ2.getFilename())
+    print("File: " + succ2.getFilename())
+    print("Name: " + succ2.getShowName())
+    print("Season: " + succ2.getSeason())
+    print("Episode: " + succ2.getEpisode())
+    print("SeasonEpisode: " + succ2.getSeasonEpisode())
+    print("Quality: " + succ2.getQuality())
+    print("Ext: " + succ2.getFileExt())
