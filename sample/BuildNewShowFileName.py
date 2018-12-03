abd0cfa1c77484a01d80eca4a88b78d5997a901b
Adam Spann <adam_spann@hotmail.com>
2018-12-03 22:26:34 +0900

    updated BuildNewShowFileName.py to import parser and use the parser object

--- sample/BuildNewShowFileName.py
+++ sample/BuildNewShowFileName.py
@@ -4,13 +4,13 @@
 import sys
 sys.path.insert(0, "..")
 
-from tvshowfile import tvshowfile
+from tvshowfile import parser
 
-test1 = tvshowfile.Parser("v.2009.S01E13.the.title.avi")
-test2 = tvshowfile.Parser("S.W.A.T.2018.S02E04.The.Dash.avi")
-test3 = tvshowfile.Parser("Castle (2015) S02E12.avi")
+test1 = parser.Parser("v.2009.S01E13.the.title.avi")
+test2 = parser.Parser("S.W.A.T.2018.S02E04.The.Dash.avi")
+test3 = parser.Parser("Castle (2015) S02E12.avi")
 
-# Example 1 
+# Example 1
 if test1.wasParsed:
     #Start building to filename
     newFileName = test1.getShowNameOnly().capitalize()
@@ -20,7 +20,7 @@ if test1.wasParsed:
     newFileName += test1.getFileExt()
     print("Old File: " + test1.filename + "; New File: " + newFileName)
 
-# Example 2 
+# Example 2
 if test2.wasParsed:
     #Start building to filename
     newFileName = test2.getShowNameOnly()
@@ -30,7 +30,7 @@ if test2.wasParsed:
     newFileName += test2.getFileExt()
     print("Old File: " + test2.filename + "; New File: " + newFileName)
 
-# Example 3 
+# Example 3
 if test3.wasParsed:
     #Start building to filename
     newFileName = test3.getShowNameOnly()
