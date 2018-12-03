2eb3ee3d08bd1aa333ce2257d9fc36560d457c63
Adam Spann <adam_spann@hotmail.com>
2018-12-03 22:10:03 +0900

    added import from tvshowfile for module file importer.py

--- tests/context.py
+++ tests/context.py
@@ -2,5 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from tvshowfile import Parser
+from tvshowfile import parser
 from tvshowfile import patterns
+from tvshowfile import importer
