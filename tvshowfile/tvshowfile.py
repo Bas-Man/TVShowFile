88f2965265f81555d4a50d5e6d95892c2976b7fc
Adam Spann <adam_spann@hotmail.com>
2018-12-03 22:05:09 +0900

    Removed tvshowfile Class from tvshowfile.py and moved it to importer.py This class might be renamed once development resumes on this part of the module.

--- tvshowfile/tvshowfile.py
+++ tvshowfile/tvshowfile.py
@@ -9,27 +9,6 @@ from .patterns import regex_SXEX, regex_name_only, regex_YEAR, regex_quality
 modPath = os.path.abspath(__file__)
 modDirPath = os.path.dirname(modPath)
 
-class TVShowFile:
-
-    def __init__(self,SourceFile):
-        self.SourceFile = SourceFile
-
-        # Should import platform and determine OS to know how to handle windows path
-        # if windows should import ntpath and use ntpath.split()
-        # see https://stackoverflow.com/questions/110362/how-can-i-find-the-current-os-in-python
-        # More reading required to know if this is actually needed.
-        self.absPath, self.filename = os.path.split(SourceFile)
-
-    def folderExists(self):
-        return os.path.isdir(self.absPath)
-
-    def fileExists(self):
-        # Return
-        return os.path.exists(self.SourceFile)
-
-    def FilePath(self):
-        return self.SourceFile
-
 class Parser:
 
     '''
