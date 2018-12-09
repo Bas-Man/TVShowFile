222d1707db3398d1e9bbf41434b05729bebddbb7
Adam Spann <adam_spann@hotmail.com>
2018-12-09 15:04:32 +0900

    additional test coverage.

---
+++ tests/071showNameOnlySXEXEX_test.py
@@ -0,0 +1,43 @@
+import unittest
+from context import parser
+
+
+class TVShowFileParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = parser.Parser("test.S01E01E02.avi")
+
+    def tearDown(self):
+        self.filename = None
+
+    def testObjValuesSet(self):
+        self.assertEqual(self.filename.showName,"test")
+        self.filename.getShowNameOnly()
+        self.assertEqual(self.filename.showNameOnly,"test")
+        self.assertEqual(self.filename.season, "01")
+        self.assertIsNone(self.filename.episode)
+        self.assertEqual(self.filename.firstEpisode,"01")
+        self.assertEqual(self.filename.lastEpisode, "02")
+        self.assertEqual(self.filename.seasonEpisode, "S01E01E02")
+        self.assertEqual(self.filename.fileExt,"avi")
+
+        print("\nName: " + self.filename.getShowName())
+        print("Season: " + self.filename.getSeason())
+        print("First Episode: " + self.filename.getFirstEpisode())
+        print("Last Episode: " + self.filename.getLastEpisode())
+        print("SeasonEpisode: " + self.filename.getSeasonEpisode())
+        print("Ext: " + self.filename.getFileExt())
+        print("Year: " + self.filename.getYear())
+        print("ShowNameOnly: " + self.filename.getShowNameOnly())
+
+    def testgetShowData(self):
+        self.assertTrue(self.filename.getShowData())
+
+    def testgetShowNameOnly(self):
+        self.assertEqual(self.filename.getShowNameOnly(), 'test')
+
+    def testisMultiEpisode(self):
+        self.assertTrue(self.filename.isMultiEpisode())
+
+if __name__ == '__main__':
+    unittest.main()
