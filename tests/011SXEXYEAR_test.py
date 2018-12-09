c71ec740e1986ed46e83a6a0c4239777e2364d3b
Adam Spann <adam_spann@hotmail.com>
2018-12-09 14:55:07 +0900

    Added more specific tests for parsing. Adding additional coverage

---
+++ tests/011SXEXYEAR_test.py
@@ -0,0 +1,45 @@
+import unittest
+from context import parser
+
+class TVShowFileParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = parser.Parser("test.2018.s01E01.1080p.avi")
+
+    def tearDown(self):
+        self.filename = None
+
+    def testObjValuesSet(self):
+        self.assertEqual(self.filename.showName,"test.2018")
+        self.assertEqual(self.filename.season, "01")
+        self.assertEqual(self.filename.episode,"01")
+        self.assertEqual(self.filename.seasonEpisode, "S01E01")
+        self.assertEqual(self.filename.fileExt,"avi")
+        self.assertIsNone(self.filename.firstEpisode)
+        self.assertIsNone(self.filename.lastEpisode)
+        self.assertEqual(self.filename.year, '2018')
+        self.assertEqual(self.filename.resolution,"1080p")
+        self.assertTrue(self.filename.wasParsed)
+        print("\nFile: " + self.filename.getFilename())
+        print("Name: " + self.filename.getShowName())
+        print("Season: " + self.filename.getSeason())
+        print("Episode: " + self.filename.getEpisode())
+        print("SeasonEpisode: " + self.filename.getSeasonEpisode())
+        print("Resolution: " + self.filename.getResolution())
+        print("Ext: " + self.filename.getFileExt())
+        if self.filename.wasParsed():
+            print("wasParsed is True")
+
+
+    def testgetShowData(self):
+        self.assertTrue(self.filename.getShowData())
+
+    def testhasYear(self):
+        self.assertTrue(self.filename.year,
+            "Year is True as filename does not contain a Year")
+
+    def testisMultiEpisode(self):
+        self.assertFalse(self.filename.multiEpisode)
+
+if __name__ == '__main__':
+    unittest.main()
