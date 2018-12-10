502848871b9a810f566a6c5e289505af1e6a3458
Adam Spann <adam_spann@hotmail.com>
2018-12-10 22:50:35 +0900

    added import for exceptman module

--- tests/context.py
+++ tests/context.py
@@ -5,3 +5,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from tvshowfile import parser
 from tvshowfile import patterns
 from tvshowfile import importer
+from tvshowfile import exceptman
