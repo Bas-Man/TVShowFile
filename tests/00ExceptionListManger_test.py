b8b7aaf18d56ae58054742abc3e95b7cd9364103
Adam Spann <adam_spann@hotmail.com>
2018-12-10 22:58:24 +0900

    Initial testcase for ExceptionListManager

---
+++ tests/00ExceptionListManger_test.py
@@ -0,0 +1,14 @@
+import unittest
+from context import exceptman
+
+class ExceptmanTests(unittest.TestCase):
+
+    def setUp(self):
+        self.handle = exceptman.ExceptionListManager()
+
+    def testObjVariblesNone(self):
+        print(self.handle.getPath())
+        print(self.handle.loadExceptionList())
+
+if __name__ == '__main__':
+    unittest.main()
