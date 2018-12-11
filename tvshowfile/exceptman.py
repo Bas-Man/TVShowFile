32afdfdf31e0db7b6affd68ecd7caf07c4930902
Adam Spann <adam_spann@hotmail.com>
2018-12-11 15:50:08 +0900

    Added DocStrings to methods in ExceptionListManager

--- tvshowfile/exceptman.py
+++ tvshowfile/exceptman.py
@@ -15,12 +15,24 @@ class ExceptionListManager:
         self.file = "exceptionlist.json"
 
     def getPath(self):
+        '''
+            Return the path to data directory.
+            rtype: Str
+        '''
         return self.path
 
     def getFileName(self):
+        '''
+            Return the name of the data file.
+            rtype: Str
+        '''
         return self.file
 
     def loadExceptionList(self):
+        '''
+            Load the json ExceptionList data from the json file.
+            rtype: ExceptionList of Dict
+        '''
 
         with open(self.path + self.file, 'r') as fhandle:
             ExceptList = json.load(fhandle)
