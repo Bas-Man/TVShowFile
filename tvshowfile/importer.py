b788565522f4808716f37a6f830b2ebe3f85f4d6
Adam Spann <adam_spann@hotmail.com>
2018-12-11 17:02:39 +0900

    Updated comments in importer.py

--- tvshowfile/importer.py
+++ tvshowfile/importer.py
@@ -3,12 +3,14 @@ import os
 modPath = os.path.abspath(__file__)
 modDirPath = os.path.dirname(modPath)
 
+
 class TVShowFile:
 
-    def __init__(self,SourceFile):
+    def __init__(self, SourceFile):
         self.SourceFile = SourceFile
 
-        # Should import platform and determine OS to know how to handle windows path
+        # Should import platform and determine OS to know how to handle windows
+        # path
         # if windows should import ntpath and use ntpath.split()
         # see https://stackoverflow.com/questions/110362/how-can-i-find-the-current-os-in-python
         # More reading required to know if this is actually needed.
