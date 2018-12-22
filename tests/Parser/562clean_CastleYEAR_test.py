c23157a4d5e51a1fa70d9cef55eaafd390c27cb5
Adam Spann <adam_spann@hotmail.com>
2018-12-22 16:06:10 +0900

    Complete migration of testing for parser.py
    Tests are all completely succesfully.
    Most get methods have been converted to properties.

--- tests/Parser/562clean_CastleYEAR_test.py
+++ tests/Parser/562clean_CastleYEAR_test.py
@@ -1,6 +1,7 @@
 import unittest
 from context import parser
 
+
 class TVShowFileParserTests(unittest.TestCase):
 
     def setUp(self):
@@ -10,31 +11,31 @@ class TVShowFileParserTests(unittest.TestCase):
         self.filename = None
 
     def testObjValuesSet(self):
-        self.assertEqual(self.filename.showName,"castle.2009")
-        self.assertEqual(self.filename.season, "01")
-        self.assertEqual(self.filename.episode,"01")
-        self.assertEqual(self.filename.seasonepisode, "S01E01")
-        self.assertEqual(self.filename._fileExt,"avi")
+        self.assertEqual(self.filename._showName, "castle.2009")
+        self.assertEqual(self.filename._season, "01")
+        self.assertEqual(self.filename._episode, "01")
+        self.assertEqual(self.filename.seasonEpisode, "S01E01")
+        self.assertEqual(self.filename._fileExt, "avi")
         self.assertIsNone(self.filename.firstEpisode)
         self.assertIsNone(self.filename.lastEpisode)
-        self.assertEqual(self.filename.getShowNameOnly(),"castle")
+        self.assertEqual(self.filename.getShowNameOnly(), "castle")
         self.assertTrue(self.filename.wasParsed)
-        print("\nFile: " + self.filename.getFilename())
-        print("Name: " + self.filename.getShowName())
+        print("\nFile: " + self.filename.fileName)
+        print("Name: " + self.filename.showName)
         print("ShowNameOnly: " + self.filename.getCleanShowName())
         print("Season: " + self.filename.season)
-        print("Episode: " + self.filename.getEpisode())
-        print("SeasonEpisode: " + self.filename.seasonepisode)
-        print("Ext: " + self.filename.fileext)
+        print("Episode: " + self.filename.episode)
+        print("SeasonEpisode: " + self.filename.seasonEpisode)
+        print("Ext: " + self.filename.fileExt)
         if self.filename.wasParsed:
             print("wasParsed is True")
-
 
     def testgetShowData(self):
         self.assertTrue(self.filename.getShowData())
 
     def testgetCleanShowName(self):
-        self.assertTrue(self.filename.getCleanShowName(),"castle")
+        self.assertTrue(self.filename.getCleanShowName(), "castle")
+
 
 if __name__ == '__main__':
     unittest.main()
