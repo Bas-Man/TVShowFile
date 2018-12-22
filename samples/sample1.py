4b9ba43da19cf2f272a3d0d048c8fef24c9e890e
Adam Spann <adam_spann@hotmail.com>
2018-12-22 19:19:36 +0900

    sample1.py updated
    This code has been updated to work with the move to properties

--- samples/sample1.py
+++ samples/sample1.py
@@ -11,28 +11,28 @@ succ2 = parser.Parser("test.S01E01.1080p.avi")
 
 # Example 1 testing that file was able to be parsed or not
 if not fail.wasParsed:
-    print("Unable to parse file: " + fail.getFilename())
+    print("Unable to parse file: " + fail.fileName + "\n")
 
 # Example 1 Successful parsing of file
 if succ.wasParsed:
-    print("Able to parse file: " + succ.getFilename())
-    print("File: " + succ.getFilename())
-    print("Name: " + succ.getShowName())
+    print("Able to parse file: " + succ.fileName)
+    print("File: " + succ.fileName)
+    print("Name: " + succ.showName)
     print("Season: " + succ.season)
-    print("Episode: " + succ.getEpisode())
-    print("SeasonEpisode: " + succ.seasonepisode)
+    print("Episode: " + succ.episode)
+    print("SeasonEpisode: " + succ.seasonEpisode)
     print("Resolution: " + succ.getResolution())
-    print("Ext: " + succ.fileext)
+    print("Ext: " + succ.fileExt)
 
 print("\n\n")
 
 # Example 2 Successful parsing of file
 if succ2.wasParsed:
-    print("Able to parse file: " + succ2.getFilename())
-    print("File: " + succ2.getFilename())
-    print("Name: " + succ2.getShowName())
+    print("Able to parse file: " + succ2.fileName)
+    print("File: " + succ2.fileName)
+    print("Name: " + succ2.showName)
     print("Season: " + succ2.season)
-    print("Episode: " + succ2.getEpisode())
-    print("SeasonEpisode: " + succ2.seasonepisode)
+    print("Episode: " + succ2.episode)
+    print("SeasonEpisode: " + succ2.seasonEpisode)
     print("Resolution: " + succ2.getResolution())
-    print("Ext: " + succ2.fileext)
+    print("Ext: " + succ2.fileExt)
