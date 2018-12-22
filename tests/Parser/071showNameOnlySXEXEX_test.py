8da13d873443ece480be075e7e4fe98c9581189c
Adam Spann <adam_spann@hotmail.com>
2018-12-22 15:40:16 +0900

    Part way through updating tests to use property values

--- tests/Parser/071showNameOnlySXEXEX_test.py
+++ tests/Parser/071showNameOnlySXEXEX_test.py
@@ -11,23 +11,23 @@ class TVShowFileParserTests(unittest.TestCase):
         self.filename = None
 
     def testObjValuesSet(self):
-        self.assertEqual(self.filename.showName,"test")
+        self.assertEqual(self.filename.showName, "test")
         self.filename.getShowNameOnly()
-        self.assertEqual(self.filename.showNameOnly,"test")
-        self.assertEqual(self.filename.season, "01")
-        self.assertIsNone(self.filename.episode)
-        self.assertEqual(self.filename.firstEpisode,"01")
+        self.assertEqual(self.filename.showNameOnly, "test")
+        self.assertEqual(self.filename._season, "01")
+        self.assertIsNone(self.filename._episode)
+        self.assertEqual(self.filename.firstEpisode, "01")
         self.assertEqual(self.filename.lastEpisode, "02")
-        self.assertEqual(self.filename.seasonepisode, "S01E01E02")
-        self.assertEqual(self.filename._fileExt,"avi")
+        self.assertEqual(self.filename.seasonEpisode, "S01E01E02")
+        self.assertEqual(self.filename._fileExt, "avi")
 
-        print("\nName: " + self.filename.getShowName())
+        print("\nName: " + self.filename.showName)
         print("Season: " + self.filename.season)
         print("First Episode: " + self.filename.getFirstEpisode())
         print("Last Episode: " + self.filename.getLastEpisode())
-        print("SeasonEpisode: " + self.filename.seasonepisode)
-        print("Ext: " + self.filename.fileext)
-        print("Year: " + self.filename.getYear())
+        print("SeasonEpisode: " + self.filename.seasonEpisode)
+        print("Ext: " + self.filename.fileExt)
+        print("Year: " + self.filename.year)
         print("ShowNameOnly: " + self.filename.getShowNameOnly())
 
     def testgetShowData(self):
@@ -37,7 +37,8 @@ class TVShowFileParserTests(unittest.TestCase):
         self.assertEqual(self.filename.getShowNameOnly(), 'test')
 
     def testisMultiEpisode(self):
-        self.assertTrue(self.filename.isMultiEpisode())
+        self.assertTrue(self.filename.isMultiEpisode)
+
 
 if __name__ == '__main__':
     unittest.main()
