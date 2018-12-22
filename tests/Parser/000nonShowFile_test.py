8eddc73542880555c130fdaba2fbdfbc89fe8456
Adam Spann <adam_spann@hotmail.com>
2018-12-22 15:07:26 +0900

    Migrating many methods to properties
    Retesting unit tests to ensure they pass.
    Commencing testing run.

--- tests/Parser/000nonShowFile_test.py
+++ tests/Parser/000nonShowFile_test.py
@@ -10,19 +10,19 @@ class TVShowFileParserTests(unittest.TestCase):
         self.filename = None
 
     def testObjVariblesNone(self):
-        self.assertIsNone(self.filename.showName)
+        self.assertIsNone(self.filename._showName)
         self.assertIsNone(self.filename._season)
-        self.assertIsNone(self.filename.episode)
-        self.assertIsNone(self.filename.seasonepisode)
+        self.assertIsNone(self.filename._episode)
+        self.assertIsNone(self.filename._seasonEpisode)
         self.assertIsNone(self.filename._fileExt)
         self.assertIsNone(self.filename.resolution)
-
 
     def testgetShowData(self):
         self.assertFalse(self.filename.getShowData())
 
     def testFileNotValid(self):
         self.assertFalse(self.filename.Parsed)
+
 
 if __name__ == '__main__':
     unittest.main()
