8c8313cb9200d4e9c4912b093349bf3c63258ce3
Adam Spann <adam_spann@hotmail.com>
2018-12-25 23:20:31 +0900

    command line interface and display feature
    Started work on a basic command line interface.
    
    Able to display the contents of the dictionary and also show contents by 
    a given key. If the key is not valid a simple message is displayed.
    
    q to exit the main loop.

--- tvshowfile/exceptman_utility.py
+++ tvshowfile/exceptman_utility.py
@@ -1,11 +1,12 @@
 import exceptman
+import sys
 
 
 def setup():
     global ExMan
     ExMan = exceptman.ExceptionListManager()
     ExMan.loadExceptionList()
-    return ExMan.exportList()
+    # return ExMan.exportList()
 
 
 def print_dict(d):
@@ -17,11 +18,39 @@ def print_dict(d):
             print("\t{0} : {1}".format(k, v))
 
 
+def print_by_key(d, key):
+    if d.hasKey(key):
+        print("Key: {}".format(key))
+        print("Name: {}".format(d.ExceptList[key].get('name')))
+        print("KeepPeriods is {}".format(
+            d.ExceptList[key].get('keepPeriods', 'false')))
+    else:
+        print("There is no key: {}".format(key))
+
+
+def list_commands():
+    print("Commands List:")
+    print("\tQ/q to quit")
+    print("\tlist to list all keys and values")
+    print("\tkey list values for given key")
+    print("")
+
+
 def main():
-    ExceptList = setup()
-    print_dict(ExceptList)
-    print("Key: s.w.a.t")
-    print_dict(ExceptList['s.w.a.t'])
+    run = True
+    list_commands()
+    while run:
+        command = input("Enter Command: ")
+        if command == 'q':
+            run = False
+            sys.exit()
+            break
+        elif command == "list":
+            print_dict(ExMan.ExceptList)
+
+        elif command == "key":
+            subcommand = input("Enter key: ")
+            print_by_key(ExMan, subcommand)
 
 
 if __name__ == '__main__':
