e831466d305dcdcd0ae7dae69f8966f3c2b2d7d9
Adam Spann <Bas-Man@users.noreply.github.com>
2019-02-12 00:06:09 +0900

    Created test 600 for Parser
    Started work on parsing show by date
    example Jimmy Fallon, Jeopardy

---
+++ tests/Parser/600byDate01_test.py
@@ -0,0 +1,36 @@
+import unittest
+from context import parser
+
+
+class TVShowFileParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = parser.Parser("Jimmy.Fallon.2019.01.29.Matthew.Broderick.720p.HDTV.x264-SORNY[eztv].mkv")
+
+    def tearDown(self):
+        self.filename = None
+
+    def testWasParsed(self):
+        self.assertTrue(self.filename.wasParsed)
+
+    def testObjValuesSet(self):
+        self.assertEqual(self.filename._showName, "Jimmy.Fallon")
+        self.assertEqual(self.filename._fileExt, "mkv")
+        self.assertEqual(self.filename.getShowNameOnly(), "Jimmy.Fallon")
+        self.assertTrue(self.filename.wasParsed)
+        print("\nFile: " + self.filename.fileName)
+        print("Name: " + self.filename.showName)
+        # print("ShowNameOnly: " + self.filename.getCleanShowName())
+        print("Ext: " + self.filename.fileExt)
+        if self.filename.wasParsed:
+            print("wasParsed is True")
+
+    def testgetShowData(self):
+        self.assertTrue(self.filename.getShowData())
+
+    def testgetCleanShowName(self):
+        self.assertTrue(self.filename.getCleanShowName(), "jimmy fallon")
+
+
+if __name__ == '__main__':
+    unittest.main()
