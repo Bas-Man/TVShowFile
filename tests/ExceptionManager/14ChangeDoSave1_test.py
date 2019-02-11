9a0c4482c63e73727038ed0e0d07eb69430333a6
Adam Spann <Bas-Man@users.noreply.github.com>
2019-02-11 23:36:41 +0900

    updated successful save test to not need arugment

--- tests/ExceptionManager/14ChangeDoSave1_test.py
+++ tests/ExceptionManager/14ChangeDoSave1_test.py
@@ -6,21 +6,14 @@ class ExceptmanTests(unittest.TestCase):
 
     def setUp(self):
         self.handle = exceptman.ExceptionListManager()
-        self.list = self.handle.loadExceptionList()
+        self.handle.loadExceptionList()
         self.handle._updated = True
 
-        self.expected = {}
-        self.expected['s.w.a.t'] = {}
-        self.expected['s.w.a.t']['name'] = 'S.W.A.T'
-        self.expected['s.w.a.t']['keepPeriods'] = True
-        self.expected['the.4400'] = {}
-        self.expected['the.4400']['name'] = 'The 4400'
-        self.expected['the.4400']['keepPeriods'] = False
-        self.expected['doctor who'] = {}
-        self.expected['doctor who']['name'] = 'Doctor Who'
+        self.handle.ExceptList['doctor who'] = {}
+        self.handle.ExceptList['doctor who']['name'] = 'Doctor Who'
 
     def testExceptListNotSaved(self):
-        self.assertTrue(self.handle.saveExceptionList(self.expected))
+        self.assertTrue(self.handle.saveExceptionList())
 
 
 if __name__ == '__main__':
