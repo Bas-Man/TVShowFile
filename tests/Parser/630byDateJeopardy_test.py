56f7f4d9f1808dc6639d590681308d6cad44dd35
Adam Spann <Bas-Man@users.noreply.github.com>
2019-02-12 22:29:43 +0900

    added 630byDateJeopardy_test.py

---
+++ tests/Parser/630byDateJeopardy_test.py
@@ -0,0 +1,39 @@
+import unittest
+from context import parser
+
+
+class TVShowFileParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = parser.Parser("Jeopardy.2019.01.30.720p.HDTV.x264[eztv].mkv")
+
+    def tearDown(self):
+        self.filename = None
+
+    def testWasParsed(self):
+        self.assertTrue(self.filename.wasParsed)
+
+    def testObjValuesSet(self):
+        self.assertEqual(self.filename._showName, "Jeopardy")
+        self.assertEqual(self.filename._fileExt, "mkv")
+        self.assertEqual(self.filename.getShowNameOnly(), "Jeopardy")
+        self.assertTrue(self.filename.wasParsed)
+        print("\nFile: " + self.filename.fileName)
+        print("Name: " + self.filename.showName)
+        print("Year: " + self.filename.year)
+        print("Month: " + self.filename.month)
+        print("Date: " + self.filename.date)
+        print("ShowNameOnly: " + self.filename.getCleanShowName())
+        print("Ext: " + self.filename.fileExt)
+        if self.filename.wasParsed:
+            print("wasParsed is True")
+
+    def testgetShowData(self):
+        self.assertTrue(self.filename.getShowData())
+
+    def testgetCleanShowName(self):
+        self.assertEqual(self.filename.getCleanShowName(), "Jeopardy")
+
+
+if __name__ == '__main__':
+    unittest.main()
